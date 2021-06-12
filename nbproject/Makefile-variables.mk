881c53f885d0eab992baf2eba27364a65a3e29ec
u <u@aditya-hppavilion15notebookpc>
2021-06-12 15:31:55 +0530

    BL Project modified for BLGpsApp

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# uart configuration
+CND_ARTIFACT_DIR_uart=dist/uart/production
+CND_ARTIFACT_NAME_uart=ex_boot_uart_pic32mm.production.hex
+CND_ARTIFACT_PATH_uart=dist/uart/production/ex_boot_uart_pic32mm.production.hex
+CND_PACKAGE_DIR_uart=${CND_DISTDIR}/uart/package
+CND_PACKAGE_NAME_uart=exbootuartpic32mm.tar
+CND_PACKAGE_PATH_uart=${CND_DISTDIR}/uart/package/exbootuartpic32mm.tar
